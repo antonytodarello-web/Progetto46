67cfddd730d668b281f0fffab65fd232c4a0b87a
Antonio Todarello-web <antonytodarello@gmail.com>
2026-02-06 16:17:54 +0100

    agg

---
+++ progetto46/src/main/java/antonio/progetto46/Progetto46Application.java
@@ -0,0 +1,13 @@
+package antonio.progetto46;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progetto46Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progetto46Application.class, args);
+	}
+
+}
---
+++ progetto46/src/test/java/antonio/progetto46/Progetto46ApplicationTests.java
@@ -0,0 +1,13 @@
+package antonio.progetto46;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Progetto46ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
